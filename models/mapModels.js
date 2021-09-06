2409645a91655c98a7aa0ce5da9e4446441fcbea
Philip Tronser <tronser@web.de>
2021-09-06 19:53:42 +0200

    instructed

--- models/mapModels.js
+++ models/mapModels.js
@@ -1,16 +1,23 @@
 import mongoose from 'mongoose';
-const { Schema, model , Types:{ObjectId} } = mongoose;
+const {
+  Schema,
+  model,
+  Types: { ObjectId }
+} = mongoose;
 
 const mapSchema = new Schema({
   setting: { type: ObjectId, ref: 'Settings', required: true },
-  marker: [{type: ObjectId, ref: 'Marker'}],
-  type: { type: String, required: true },
+  marker: [{ type: ObjectId, ref: 'Marker' }],
   title: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
   bounds: [[Number]],
-  plane: { type: String, enum: ['Galaxy', 'Solar System', 'Planet', 'Continent', 'Country', 'City', 'Building'], required: true },
-  visibility:[{ type: ObjectId, ref: 'User'  }]
+  plane: {
+    type: String,
+    enum: ['Galaxy', 'Solar System', 'Planet', 'Continent', 'Country', 'City', 'Building'],
+    required: true
+  },
+  visibility: [{ type: ObjectId, ref: 'User' }]
 });
 
 export default model('Map', mapSchema);
